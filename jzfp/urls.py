3baa69cda0e9ecbc79eef43f0d01437b8367151a
zlldt <zlldtis@gmail.com>
2018-06-10 16:14:13 +0800

    remove .idea, modify input check, errormsg,

--- jzfp/urls.py
+++ jzfp/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
     path('', views.search_form),
     path('<int:id_num>/', views.detail, name='detail'),
     path('search-form/', views.search_form),
--- jzfp/views.py
+++ jzfp/views.py
@@ -5,12 +5,12 @@ from .models import Person
 from django.db.models import Q
 # Create your views here.
 
-
-def index(request):
-    person_list = Person.objects.order_by('sn')
-    template = loader.get_template('jzfp/index.html')
-    context = {'person_list': person_list, }
-    return HttpResponse(template.render(context, request))
+# 未使用，注释掉
+# def index(request):
+#     person_list = Person.objects.order_by('sn')
+#     template = loader.get_template('jzfp/index.html')
+#     context = {'person_list': person_list, }
+#     return HttpResponse(template.render(context, request))
 
 
 def detail(request, id_num):
@@ -24,13 +24,17 @@ def search_form(request):
 
 
 def search(request):
+    errmsg = []
     request.encoding = 'utf-8'
     if 'q' in request.GET:
-        person_list = Person.objects.filter(Q(id_num__contains=request.GET['q'])|Q(name__icontains=request.GET['q']))
-        # return render(request, 'jzfp/detail.html',{'person': person})
-        template = loader.get_template('jzfp/index.html')
-        context = {'person_list': person_list, }
-        return HttpResponse(template.render(context, request))
-    else:
-        message = '提交内容为空'
-    return HttpResponse(message)
+        q = request.GET['q']
+        if not q:
+            errmsg.append('提交内容为空')
+        elif len(q) > 20:
+            errmsg.append('输入字符串超长')
+        else:
+            person_list = Person.objects.filter(Q(id_num__contains=request.GET['q']) | Q(name__icontains=request.GET['q']))
+            template = loader.get_template('jzfp/index.html')
+            context = {'person_list': person_list, }
+            return HttpResponse(template.render(context, request))
+        return render_to_response('jzfp/search.html', {'errmsg': errmsg})
